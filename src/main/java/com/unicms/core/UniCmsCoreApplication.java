1cb3ed503a3cfc8b77c4ebd07ab1c553d4520580
Dmitriy S. Porollo <d.porollo@gmail.com>
2019-03-28 01:30:54 +0300

    Initial commit

---
+++ src/main/java/com/unicms/core/UniCmsCoreApplication.java
@@ -0,0 +1,13 @@
+package com.unicms.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniCmsCoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UniCmsCoreApplication.class, args);
+    }
+
+}
