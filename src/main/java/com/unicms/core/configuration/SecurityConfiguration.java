b12a1dedd67c8aced0b9c5f5762e582923654a2d
Dmitriy S. Porollo <d.porollo@gmail.com>
2019-04-21 23:21:16 +0300

    Fixed Bugs

---
+++ src/main/java/com/unicms/core/configuration/SecurityConfiguration.java
@@ -0,0 +1,37 @@
+/*
+package com.unicms.core.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@EnableWebSecurity
+public class SecurityConfiguration implements WebMvcConfigurer {
+
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers("/resources/**", "/signup", "/").permitAll()
+                .antMatchers("/admin/**").hasRole("ADMIN")
+                .antMatchers("/db/**").access("hasRole('ADMIN') and hasRole('DBA')")
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .loginPage("/login")
+                .permitAll();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() throws Exception {
+        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
+        manager.createUser(User.withDefaultPasswordEncoder().username("admin").password("secret").roles("ADMIN").build());
+        return manager;
+    }
+}
+
+
+*/
--- src/main/java/com/unicms/core/controller/CMSController.java
+++
@@ -1,4 +0,0 @@
-package com.unicms.core.controller;
-
-public class CMSController {
-}
