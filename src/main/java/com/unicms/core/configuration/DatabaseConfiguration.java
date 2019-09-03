076ade56dc422944ef5cc567f23662c1c592fbee
Dmitriy S. Porollo <d.porollo@gmail.com>
2019-09-04 01:36:34 +0300

    Some Fixes

--- src/main/java/com/unicms/core/configuration/DatabaseConfiguration.java
+++ src/main/java/com/unicms/core/configuration/DatabaseConfiguration.java
@@ -1,12 +1,17 @@
 package com.unicms.core.configuration;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 
 import javax.sql.DataSource;
 
+@Configuration
 public class DatabaseConfiguration {
 
     @Bean
@@ -20,5 +25,20 @@ public class DatabaseConfiguration {
     @ConfigurationProperties(prefix="spring.pg-datasource")
     public DataSource secondaryDataSource() {
         return DataSourceBuilder.create().build();
+    }
+
+    @Bean(name="tm1")
+    @Autowired
+    @Primary
+    DataSourceTransactionManager tm1(@Qualifier("primaryDataSource") DataSource datasource) {
+        DataSourceTransactionManager txm  = new DataSourceTransactionManager(datasource);
+        return txm;
+    }
+
+    @Bean(name="tm2")
+    @Autowired
+    DataSourceTransactionManager tm2(@Qualifier ("secondaryDataSource") DataSource datasource) {
+        DataSourceTransactionManager txm  = new DataSourceTransactionManager(datasource);
+        return txm;
     }
 }
