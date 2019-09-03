0d57a4d3528ddb69eb2482791f0fcf88b68cde0e
Dmitriy S. Porollo <d.porollo@gmail.com>
2019-09-04 01:30:25 +0300

    Added second datasource (PostgreSQL)

--- src/main/java/com/unicms/core/configuration/DatabaseConfiguration.java
+++ src/main/java/com/unicms/core/configuration/DatabaseConfiguration.java
@@ -1,4 +1,24 @@
 package com.unicms.core.configuration;
 
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
+
+import javax.sql.DataSource;
+
 public class DatabaseConfiguration {
+
+    @Bean
+    @Primary
+    @ConfigurationProperties(prefix="spring.datasource")
+    public DataSource primaryDataSource() {
+        return DataSourceBuilder.create().build();
+    }
+
+    @Bean
+    @ConfigurationProperties(prefix="spring.pg-datasource")
+    public DataSource secondaryDataSource() {
+        return DataSourceBuilder.create().build();
+    }
 }
