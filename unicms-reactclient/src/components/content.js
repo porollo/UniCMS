1b33c00cbed90547b634612c791d9c18e8e195e6
porollo <d.porollo@gmail.com>
2020-07-09 04:14:24 +0300

    Some little works

---
+++ unicms-reactclient/src/components/content.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import CustomizedTables from "./table";
+
+export const Content = () => (
+    <CustomizedTables/>
+)
--- unicms-reactclient/src/components/header.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-export const Header = () => (
-    <div>Header Component</div>
-)
